e15d30eafdb4179359da79aa835484ae611f8b85
biddabari-dev <biddabarideveloper@gmail.com>
2024-05-19 06:02:07 -0700

    d

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -14,6 +14,7 @@
         <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/iconplugins.css">
 
         <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/style.min.css">
+{{--        <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/style.css">--}}
 
 
         <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/responsive.min.css">
