49f6d96f51c9248b52b40d7f2ffad93bcd152bfa
biddabari-dev <biddabarideveloper@gmail.com>
2024-05-24 05:23:29 -0700

    a

--- routes/front.php
+++ routes/front.php
@@ -89,7 +89,7 @@ Route::as('front.')->group(function (){
 Route::middleware('previousUrlMiddleware')->group(function (){
         Route::get('/', [BasicViewController::class, 'home'])->name('home');
         Route::get('/all-courses', [BasicViewController::class, 'allCourses'])->name('all-courses');
-        Route::get('/details/{slug}', [BasicViewController::class, 'courseDetails'])->name('course-details');
+        Route::get('/course-details/{id}/{slug?}', [BasicViewController::class, 'courseDetails'])->name('course-details');
         Route::get('/checkout/{type}/{id}/{slug?}', [BasicViewController::class, 'checkout'])->name('checkout');
 
         Route::get('/category/{id}/{slug?}', [BasicViewController::class, 'categoryCourses'])->name('category-courses');
