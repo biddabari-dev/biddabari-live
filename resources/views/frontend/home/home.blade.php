d1a1b82b56ae58ab9916dc2c40e0eec1fb0e1552
biddabari-dev <biddabarideveloper@gmail.com>
2024-05-29 16:27:13 +0600

    checkout

--- resources/views/frontend/home/home.blade.php
+++ resources/views/frontend/home/home.blade.php
@@ -216,21 +216,21 @@
                                             $date = date('Y-m-d H:i')
                                         @endphp
                                             @if($course->admission_last_date > $date)
-{{--                                                <a href="{{ route('front.checkout', ['id' => $course->id, 'slug' => $course->slug]) }}"--}}
-{{--                                                   class="btn btn-warning">কোর্সটি কিনুন</a>--}}
+                                                <a href="{{ route('front.checkout', ['type' => 'course', 'slug' => $course->slug]) }}"
+                                                   class="btn btn-warning">কোর্সটি কিনুন</a>
 
-                                            <form action="{{ route('front.place-course-order', ['course_id' => $course->id]) }}" method="post">
-                                                @csrf
-                                                <input type="hidden" name="course_id" value="{{ $course->id }}" />
-                                                <input type="hidden" name="total_amount" value="{{ $totalAmount }}" />
-                                                <input type="hidden" name="used_coupon" value="0">
-                                                <input type="hidden" name="coupon_code" value="">
-                                                <input type="hidden" name="coupon_amount" value="">
-                                                <input type="hidden" name="ordered_for" value="course">
-                                                <input type="hidden" name="rc" value="{{ $_GET['rc'] ?? '' }}">
-                                                <input type="hidden" name="payment_method" value="ssl">
-                                                <input type="submit" class="btn btn-warning" value="কোর্সটি কিনুন">
-                                            </form>
+{{--                                            <form action="{{ route('front.place-course-order', ['course_id' => $course->id]) }}" method="post">--}}
+{{--                                                @csrf--}}
+{{--                                                <input type="hidden" name="course_id" value="{{ $course->id }}" />--}}
+{{--                                                <input type="hidden" name="total_amount" value="{{ $totalAmount }}" />--}}
+{{--                                                <input type="hidden" name="used_coupon" value="0">--}}
+{{--                                                <input type="hidden" name="coupon_code" value="">--}}
+{{--                                                <input type="hidden" name="coupon_amount" value="">--}}
+{{--                                                <input type="hidden" name="ordered_for" value="course">--}}
+{{--                                                <input type="hidden" name="rc" value="{{ $_GET['rc'] ?? '' }}">--}}
+{{--                                                <input type="hidden" name="payment_method" value="ssl">--}}
+{{--                                                <input type="submit" class="btn btn-warning" value="কোর্সটি কিনুন">--}}
+{{--                                            </form>--}}
                                             @else
                                                 <a class="btn btn-warning">ভর্তির সময় শেষ</a>
                                             @endif
