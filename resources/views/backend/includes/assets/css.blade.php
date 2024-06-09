0ca11817ec723b2a7e7e873047733ebd2077759d
biddabari-dev <biddabarideveloper@gmail.com>
2024-06-09 15:39:44 +0600

    admin

--- resources/views/backend/includes/assets/css.blade.php
+++ resources/views/backend/includes/assets/css.blade.php
@@ -28,7 +28,7 @@
 <link href="{{ asset('/') }}backend/assets/css/skin-modes.css" rel="stylesheet" />
 
 <!--- FONT-ICONS CSS -->
-<link href="{{ asset('/') }}backend/assets/plugins/icons/icons.css" rel="stylesheet" />
+<link href="backend/assets/plugins/icons/icons.css" rel="stylesheet" />
 
 <!-- INTERNAL Switcher css -->
 <link href="{{ asset('/') }}backend/assets/switcher/css/switcher.css" rel="stylesheet">
